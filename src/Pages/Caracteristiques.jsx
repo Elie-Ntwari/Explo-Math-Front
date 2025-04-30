a11f8a96fc993753e9fd2c0e52c30b2171b3fff2
ElieNtwari <elientwari@gmail.com>
2025-04-30 11:35:05 +0100

    Fix Caracteristiques

--- src/Pages/Caracteristiques.jsx
+++ src/Pages/Caracteristiques.jsx
@@ -1,10 +1,12 @@
 import MyNavBar from "../components/MyNavBar";
+import Proprietes from "../components/Proprietes";
 
 function caracteristiques() {
 
     return (
       <div className="accueil-container">
         <MyNavBar />
+        <Proprietes/>
         
       </div>
   
