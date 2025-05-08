c1e722a9db141d85c8a72deb95e7e1604735d134
Jelly Maweja <jellymaweja7@gmail.com>
2025-05-08 23:37:21 +0100

    Liaison du maître atomix

--- src/components/Proprietes.jsx
+++ src/components/Proprietes.jsx
@@ -9,7 +9,7 @@ function Proprietes() {
   const { proprietes = [], nombre = 0 } = location.state || {};
   const [expandedItems, setExpandedItems] = useState({});
 console.log(proprietes)
-  const pro = Object.entries(proprietes); 
+  
   const toggleExpand = (index) => {
     setExpandedItems(prev => ({
       ...prev,
@@ -20,7 +20,7 @@ console.log(proprietes)
   return (
     <div className="timeline">
       <div className='number'><p><strong>{nombre}</strong></p></div>
-      {pro.map(([nom, item], index) => {
+      {proprietes && Object.entries(proprietes).map(([nom, item], index) => {
         const isExpanded = expandedItems[index];
         const description = item.description || '';
         const shouldTruncate = description.length > 100 && !isExpanded;
